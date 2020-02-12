4b0da887265d07f4b4c06b136f4659d6f2eef2e3
Jim Veneskey <deathwing@bigorc.com>
2020-02-11 20:06:26 -0500

    Trying to disable ALL channels

---
+++ examples/channels_off.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import io
+import sys
+import time
+import argparse
+
+import serial
+from serial.threaded import LineReader, ReaderThread
+
+parser = argparse.ArgumentParser(description='Blink the LEDs.')
+parser.add_argument('port', help="Serial port descriptor")
+parser.add_argument('--delay', '-d', help="Delay in seconds as a decimal number.", type=float, default=.5)
+args = parser.parse_args()
+
+class PrintLines(LineReader):
+    def connection_made(self, transport):
+        super(PrintLines, self).connection_made(transport)
+        print("port opened")
+
+    def handle_line(self, data):
+        print(data)
+
+    def connection_lost(self, exc):
+        print("port closed")
+
+ser = serial.Serial(args.port, baudrate=57600)
+delay = args.delay
+mode = args.mode
+with ReaderThread(ser, PrintLines) as protocol:
+    protocol.write_line("sys get ver")
+    time.sleep(.5)
+    for channel in range(0, 73):
+        print(f"Attempting to disable channel: {channel}")
+        protocol.write_line(f"mac set ch status {channel} off")
+        time.sleep(delay)
+    print("Done! (I hope)")
+    exit(1)
+    
